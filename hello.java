65d68fcaf91a7443827b340b24416c442c602aac
rushi7637 <rushikesh.patil@mitaoe.ac.in>
2023-09-10 10:08:23 +0530

    first commit

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class a
 {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hii ss");
 
     }
 }
