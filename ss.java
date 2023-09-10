4c37e5bf1ec1a985cda2452380b73b30b408975c
rushi7637 <rushikesh.patil@mitaoe.ac.in>
2023-09-10 10:48:36 +0530

    minor change commit

--- ss.java
+++ ss.java
@@ -1,8 +1,10 @@
-class b
+class ss
 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("pune town");
+        System.out.println("jalgaong town");
+
 
     }
 }
